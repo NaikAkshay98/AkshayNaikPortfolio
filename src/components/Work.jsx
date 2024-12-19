d58dcdbb7094cad4ec59526ff74e26a1d54e9a7e
NaikAkshay98 <akshay.h.naik@gmail.com>
2024-12-19 18:40:45 -0500

    Adding projects in work.js

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -26,6 +26,36 @@ const Work = () => {
                         </a>
                     </div>
                 </div>
+                
+                <div className="aspect-square bg-[url('./assets/work-6.png')] bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group">
+
+                    <div className="bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 flex items-center justify-between duration-500 group-hover:bottom-7">
+                        <div>
+                            <h2 className="font-semibold">Real Estate</h2>
+                            <p className="text-sm text-gray-700">React JS, Vite, Tailwind CSS</p>
+                        </div>
+                        <a href="https://real-estate-lovat-seven.vercel.app/" target="_blank" rel="noopener noreferrer">
+                            <div className="border rounded-full border-black w-9 aspect-square flex items-center justify-center shadow-[2px_2px_0_#000] group-hover:bg-lime-300 transition">
+                                <img src={send_icon} alt="Send Icon" className="w-5" />
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+                <div className="aspect-square bg-[url('./assets/work-7.png')] bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group">
+
+                    <div className="bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 flex items-center justify-between duration-500 group-hover:bottom-7">
+                        <div>
+                            <h2 className="font-semibold">Real Estate</h2>
+                            <p className="text-sm text-gray-700">React JS, Vite, Tailwind CSS</p>
+                        </div>
+                        <a href="https://gemini-clone-pied-theta.vercel.app/" target="_blank" rel="noopener noreferrer">
+                            <div className="border rounded-full border-black w-9 aspect-square flex items-center justify-center shadow-[2px_2px_0_#000] group-hover:bg-lime-300 transition">
+                                <img src={send_icon} alt="Send Icon" className="w-5" />
+                            </div>
+                        </a>
+                    </div>
+                </div>
 
 
                 <div className="aspect-square bg-[url('./assets/work-1.png')] bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group">
