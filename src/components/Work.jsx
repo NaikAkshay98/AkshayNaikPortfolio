a7652351916eecdca76d27c2a50264fcedf9e9a1
NaikAkshay98 <akshay.h.naik@gmail.com>
2024-12-26 18:46:00 -0500

    project

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -73,6 +73,35 @@ const Work = () => {
                     </div>
                 </div>
 
+                <div className="aspect-square bg-[url('./assets/work-16.png')] bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group">
+                    <div className="bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 flex items-center justify-between duration-500 group-hover:bottom-7">
+                        <div>
+                            <h2 className="font-semibold">Prescripto</h2>
+                            <p className="text-sm text-gray-700">React JS, Node JS, Stripe, Cloudinary API, MongoDB, Tailwind CSS</p>
+                        </div>
+                        <a href="https://prescripto-yzvq.vercel.app/" target="_blank" rel="noopener noreferrer">
+                            <div className="border rounded-full border-black w-9 aspect-square flex items-center justify-center shadow-[2px_2px_0_#000] group-hover:bg-lime-300 transition">
+                                <img src={send_icon} alt="Send Icon" className="w-5" />
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+
+                <div className="aspect-square bg-[url('./assets/work-15.png')] bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group">
+                    <div className="bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 flex items-center justify-between duration-500 group-hover:bottom-7">
+                        <div>
+                            <h2 className="font-semibold">Cryptoplace</h2>
+                            <p className="text-sm text-gray-700">React JS, CoinGecko API MongoDB, Tailwind CSS</p>
+                        </div>
+                        <a href="https://cryptoplace-lac.vercel.app/" target="_blank" rel="noopener noreferrer">
+                            <div className="border rounded-full border-black w-9 aspect-square flex items-center justify-center shadow-[2px_2px_0_#000] group-hover:bg-lime-300 transition">
+                                <img src={send_icon} alt="Send Icon" className="w-5" />
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
                 <div className="aspect-square bg-[url('./assets/work-1.png')] bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group">
 
                     <div className="bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 flex items-center justify-between duration-500 group-hover:bottom-7">
